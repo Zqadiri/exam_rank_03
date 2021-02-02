8c0e4d942caa22a003dbc911228021926273c1a9
Zaineb Qadiri <zqadiri@e1r11p8.1337.ma>
2021-02-02 18:59:02 +0100

    tsting micro paint

--- micro_paint/micro_paint.c
+++ micro_paint/micro_paint.c
@@ -6,30 +6,20 @@
 
 struct  data
 {
-    int w;
-    int h;
+    int  w;
+    int  h;
     char c;
 }       data;
 
 struct rectangle
 {
-    char r;     // the character r
+    char  r;     // the character r
     float x;    // horizontal position of the top left corner of the rectangle
     float y;    // vertical position of the top left corner of the rectangle
     float w;    // the width of the rectangle
     float h;    // the height of the rectangle
-    char c;     // the char use to draw the rectangle
+    char  c;     // the char use to draw the rectangle
 }       rec;
-
-struct Rectangle
-{
-    char  R;    // the character r
-    float X;    // horizontal position of the top left corner of the rectangle
-    float Y;    // vertical position of the top left corner of the rectangle
-    float W;    // the width of the rectangle
-    float H;    // the height of the rectangle
-    char  C;    // the char use to draw the rectangle
-}       Rec;
 
 int     quit(FILE *fp)
 {
@@ -39,12 +29,38 @@ int     quit(FILE *fp)
     return (1);
 }
 
+void    draw_empty_rectangle(struct rectangle *rec, struct data *data)
+{
+    // float x_end;
+    // float y_end;
+    // float x_start = rec->x;
+    // float y_start = rec->y;
+
+    // x_end = rec->x + rec->w;
+    // y_end = rec->y +rec->h;
+    // for (size_t x = 0; x < x_end; x++)
+    // {
+    //     for (int y = 0; y < y_end; y++)
+    //     {
+    //          result[y * rec->w + x] = rec->c;
+    //     }
+    // }
+}
+int	 check_cor(float x, float y, float rec_x, float rec_y, float width, float height)
+{
+	if (x < rec_x || x > rec_x + width || y < rec_y || y > rec_y + height)
+		return (0);
+	else if (x - rec_x < 1.0000000 || (rec_x + width) - x < 1.0000000 ||
+			y - rec_y < 1.0000000 || (rec_y + height) - y < 1.0000000)
+		return (1);
+	return (2);
+}
+
 int     main(int argc, char **argv)
 {
     char result[90000]; //300 * 300 
     struct data data;
     struct rectangle rec;
-    struct Rectangle Rec; 
     FILE *fp;
     int  ret;
 
@@ -70,33 +86,52 @@ int     main(int argc, char **argv)
     } 
     memset (result, data.c, data.w * data.h);
     /*
-    **  Draw an empty rectangle
+    **  Draw rectangle
     */
-    while ((ret = fscanf(fp, "%c %f %f %f %c\n", &rec.r, &rec.x, &rec.y,
-    &rec.w, &rec.h, &rec.c)) == 5)
+    while ((ret = fscanf(fp, "%c %f %f %f %f %c\n", &rec.r, &rec.x, &rec.y,
+    &rec.w, &rec.h, &rec.c)) == 6)
     {
-        if (rec.w <= 0 || rec.h <= 0 || rec.r != 'r')
+        printf ("%c\n", rec.r);
+        printf ("%f\n", rec.x);
+        printf ("%f\n", rec.y);
+        printf ("%f\n", rec.w);
+        printf ("%f\n", rec.h);
+        printf ("%c\n", rec.c);
+        if (rec.w <= 0 || rec.h <= 0 || (rec.r != 'r' && rec.r != 'R'))
         {
-            quit(fp);
+            quit (fp);
             return (1);
         }
-        if (Rec.W <= 0 || Rec.H <= 0 || Rec.R != 'R')
+        for (int y = 0; y < data.h; y++)
         {
-            quit(fp);
-            return (1);
+            for (int x = 0; x < data.w ; x++)
+            {
+                int i = check_cor((float)x, (float)y, rec.x, rec.y, rec.w, rec.h);
+                if (i == 1 || (i == 2 && rec.r == 'R'))
+                    result[y * data.w + x] = rec.c;
+            }
+            
         }
-        if (rec.r == 'r')
-            draw_empty_rectangle(&rec);
-        else if (Rec.R == 'R')
-            draw_filled_rectangle(&Rec);
-        /*
-        ** ... 
-        */
+        // if (rec.w > 0 && rec.h > 0 && rec.r == 'r')
+        //     draw_empty_rectangle(&rec, &data)
+        // else if (rec.w > 0 && rec.h > 0 && rec.r == 'R')
+        // {
+        //     float x_end;
+        //     float y_end;         
+        //     x_end = rec.x + rec.w;
+        //     y_end = rec.y +rec.h;
+        // }
+        // else
+        // {
+        //     quit(fp);
+        //     return (1);
+        // }
     }
     for (int i = 0; i < data.h; i++)
 	{
 		write(1, result + data.w * i, data.w);
 		write(1, "\n", 1);
 	}
+    fclose (fp);
     return (1);
 }
